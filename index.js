264297ed7084bfc2494199ee28fb241480ac25cf
DESKTOP-E99CUEM\DELL <vinaykumar@clinivalut.org>
2026-03-28 17:50:03 +0530

    changes done

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 console.log("Welcome to Node.js");
+
+
+// function greet(){
+//     console.log("hello users"+" "+this);
+// }
+
+// greet();
+
+// console.log(this);
+
+// const timer = {
+//   seconds: 0,
+//   start: function() {
+//     // Regular function here would lose 'this'
+//     setTimeout(() => {
+//       this.seconds++; 
+//       console.log(this.seconds);
+//     }, 1000);
+//   }
+// };
+// timer.start();
+// const dog = { name: "Buddy", bark: function() { console.log(this.name); }};
+// const speak = dog.bark;
+// speak(); // What is the
